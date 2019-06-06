32c93b1ddd1aed584e095b54eb5a79bd5ffdc9c4
Veanoid <36467761+Veanoid@users.noreply.github.com>
2019-06-06 12:48:12 +1000

    - Added Heat Class - Added Room Class

---
+++ Javelin/Assets/scripts/Room.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    private float RoomTemp = 0;
+
+    public bool m_isChanging;
+    public float m_multipler;
+    public bool m_currentRoom;
+    public float m_roomTemp;
+    public float m_tempTarget;
+
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_roomTemp = RoomTemp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TempRise(bool heat)
+    {
+
+    }
+}
